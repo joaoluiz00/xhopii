ad32b12166ccdc4e9f0b13b30ec48455b1992777
Gustavo <gustavobispocosta5521@gmail.com>
2024-06-17 18:57:10 -0300

    Gustavo/Banco Atualizado

--- Banco/xhopii.sql
+++ Banco/xhopii.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 12-Jun-2024 às 19:26
--- Versão do servidor: 10.4.27-MariaDB
--- versão do PHP: 8.2.0
+-- Tempo de geração: 17/06/2024 às 23:50
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cliente`
+-- Estrutura para tabela `cliente`
 --
 
 CREATE TABLE `cliente` (
@@ -38,45 +38,84 @@ CREATE TABLE `cliente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Extraindo dados da tabela `cliente`
+-- Despejando dados para a tabela `cliente`
 --
 
 INSERT INTO `cliente` (`cpf`, `nome`, `sobrenome`, `dataNascimento`, `telefone`, `email`, `senha`) VALUES
-('12345678', 'Bola', 'Almeida', '2024-06-01', '1231231234', 'bl@gmail.com', '12345'),
-('123456789', 'Gustavo ', 'Henrique ', '2024-06-03', '1892939213', 'gu@gmail.com', '1515'),
-('40308699807', 'Joao', 'Pedro', '2006-03-01', '18997304003', 'joao@gmail.com', 'Joao2006');
+('1234567890', 'joao', 'luiz', '2024-05-25', '1234567890', 'joao@gmail.com', 'joao123'),
+('126545172', 'Gustavo', 'Henrique', '2024-06-01', '125343133', 'gu123', '123');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `funcionario`
+-- Estrutura para tabela `funcionarios`
 --
 
-CREATE TABLE `funcionario` (
-  `nome` varchar(200) NOT NULL,
-  `sobrenome` varchar(200) NOT NULL,
-  `cpf` varchar(11) NOT NULL,
-  `dataNasc` date NOT NULL,
-  `telefone` varchar(11) NOT NULL,
-  `email` varchar(200) NOT NULL,
-  `salario` decimal(10,0) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+CREATE TABLE `funcionarios` (
+  `funccpf` varchar(15) NOT NULL,
+  `funcnome` varchar(250) NOT NULL,
+  `funcsobrenome` varchar(250) NOT NULL,
+  `funcdatanas` date NOT NULL,
+  `functelefone` varchar(20) NOT NULL,
+  `funcemail` varchar(200) NOT NULL,
+  `funcsalario` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Despejando dados para a tabela `funcionarios`
+--
+
+INSERT INTO `funcionarios` (`funccpf`, `funcnome`, `funcsobrenome`, `funcdatanas`, `functelefone`, `funcemail`, `funcsalario`) VALUES
+('12312311', 'Carlos', 'Medeiros', '2024-06-01', '1899743622', 'carlos123', 1000.00),
+('49879867512', 'Gustavo', 'Costa', '2006-03-14', '18997456732', 'gu@gmail.com', 3000.00);
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `produto`
+--
+
+CREATE TABLE `produto` (
+  `id` int(11) NOT NULL,
+  `nome` varchar(250) NOT NULL,
+  `fabricante` varchar(250) NOT NULL,
+  `descricao` varchar(250) NOT NULL,
+  `valor` decimal(10,2) NOT NULL,
+  `quantidade` int(11) NOT NULL,
+  `imagem` blob NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
--- Índices para tabela `cliente`
+-- Índices de tabela `cliente`
 --
 ALTER TABLE `cliente`
   ADD PRIMARY KEY (`cpf`);
 
 --
--- Índices para tabela `funcionario`
+-- Índices de tabela `funcionarios`
 --
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`cpf`);
+ALTER TABLE `funcionarios`
+  ADD PRIMARY KEY (`funccpf`);
+
+--
+-- Índices de tabela `produto`
+--
+ALTER TABLE `produto`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `produto`
+--
+ALTER TABLE `produto`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
